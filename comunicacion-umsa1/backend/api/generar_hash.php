a69adce29cad54968aaf39d776b4122f73f44945
rosio28 <rosiomasielticonachirinos@gmail.com>
2026-05-11 22:55:18 -0400

    fix: modal reset password y mejoras auth

--- comunicacion-umsa1/backend/api/generar_hash.php
+++
@@ -1,7 +0,0 @@
-<?php
-$pass = 'Admin2026!';
-$hash = password_hash($pass, PASSWORD_BCRYPT, ['cost' => 12]);
-echo "Contraseña: $pass\n";
-echo "Hash: $hash\n";
-echo "Verificación: ";
-var_dump(password_verify($pass, $hash));
